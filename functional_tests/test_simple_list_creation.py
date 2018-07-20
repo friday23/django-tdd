521a9006befe91b42c304d5b164b4b4f16162615
peter_martiniello <peter_martiniello@infosys.com>
2018-07-20 18:16:37 +1000

    Moved Fts into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,40 +1,9 @@
-from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import unittest
-import time
+from .base import FunctionalTest
 
-MAX_WAIT = 10
+class NewVistorTest(FunctionalTest):
 
-
-
-class NewVistorTest(LiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-       self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-
-                rows = table.find_elements_by_tag_name('td')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -118,41 +87,7 @@ class NewVistorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
-    def can_start_a_list_and_retrive_it_later(self):
-    #def test_can_start_a_list_and_retrive_it_later(self):
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock feathers' for row in rows),
-        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        # )
 
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
 
-        # The page updates again, and now shows both items on her list
-
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-
-        # She visits that URL - her to-do list is still there.
-
-        # Satisfied, she goes back to sleep
-
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # She notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
